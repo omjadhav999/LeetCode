81c18489c732bd7422297ed0ecec5ccc40299d56
Om Jadhav <jadhavom229@gmail.com>
2025-10-22 13:51:08 +0530

    Time: 380 ms (66.83%) | Memory: 83.4 MB (91.00%) - LeetSync

---
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int check(vector<int>& nums, int n, int t, int m){
+        long long nL = n;
+        long long tL = t;
+        int l = lower_bound(nums.begin(), nums.end(), nL) - nums.begin();
+        int h = upper_bound(nums.begin(), nums.end(), nL) - nums.begin();
+        int ll = lower_bound(nums.begin(), nums.end(), nL - tL) - nums.begin();
+        int hh = upper_bound(nums.begin(), nums.end(), nL + tL) - nums.begin();
+        int res = (hh-h) + (l-ll);
+
+    return min(m, res) + (h-l);
+    }
+
+    int maxFrequency(vector<int>& nums, int k, int numOperations){
+        int m = numOperations;
+        sort(nums.begin(), nums.end());
+        int res=1;
+        for(int i=0; i<nums.size()-1; i++){
+            res = max(check(nums, nums[i], k, m), res);
+            res = max(check(nums, nums[i]-k, k, m), res);
+            res = max(check(nums, nums[i]+k, k, m), res);
+        }
+    
+    return res;
+    }
+};
