3f3336583d79d81d47e433b1cbc6f80acf7dec23
Om Jadhav <jadhavom229@gmail.com>
2025-10-23 19:11:40 +0530

    Time: 102 ms (38.53%) | Memory: 69.3 MB (77.07%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int n=s.length(), m=n/2;
+        if(n==1 || n==2) return s;
+
+        string f = s.substr(0, n/2);
+        sort(f.begin(), f.end());
+
+        string rev = f;
+        reverse(rev.begin(), rev.end());
+
+        if((n%2)==1) f = f+s[m];
+    
+    return f+rev;
+    }
+};
