9a4ba80c622d960e0949594158855d0550be96f4
Om Jadhav <jadhavom229@gmail.com>
2025-11-04 09:43:53 +0530

    Time: 33 ms (7.35%) | Memory: 37.1 MB (31.31%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -1,28 +1,28 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     vector<int> findXSum(vector<int>& nums, int k, int x) {
-        vector<int> ans;
-        for (int i = 0; i + k <= nums.size(); i++) {
+        vector<int> res;
+        for(int i=0; i+k<=nums.size(); i++){
             unordered_map<int, int> freq;
-            for (int j = i; j < i + k; j++) freq[nums[j]]++;
+            for(int j=i; j<i+k; j++) freq[nums[j]]++;
 
             vector<pair<int, int>> items;
-            for (auto it = freq.begin(); it != freq.end(); ++it)
+            for(auto it=freq.begin(); it!=freq.end(); ++it){
                 items.push_back({it->first, it->second});
+            }
 
-            sort(items.begin(), items.end(), [](auto &a, auto &b) {
-                return a.second == b.second ? a.first > b.first : a.second > b.second;
+            sort(items.begin(), items.end(), [](auto &a, auto &b){
+                return a.second==b.second ? a.first>b.first : a.second>b.second;
             });
 
             int x_sum = 0;
-            for (int j = 0; j < items.size() && j < x; j++)
-                x_sum += items[j].first * items[j].second;
+            for(int j=0; j<items.size() && j<x; j++){
+                x_sum = x_sum + items[j].first * items[j].second;
+            }
 
-            ans.push_back(x_sum);
+            res.push_back(x_sum);
         }
-        return ans;
+
+    return res;
     }
 };
