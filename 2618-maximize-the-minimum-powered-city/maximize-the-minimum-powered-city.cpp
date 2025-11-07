ff5a7658301e1136174cf5d72acc9beb3c1d3db0
Om Jadhav <jadhavom229@gmail.com>
2025-11-07 10:10:21 +0530

    Time: 64 ms (94.64%) | Memory: 146 MB (79.49%) - LeetSync

---
+++ 2618-maximize-the-minimum-powered-city/maximize-the-minimum-powered-city.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    static bool check(long long minPower, vector<int> stations, int r, long long k, int n) {
+        long long power = reduce(stations.begin(), stations.begin()+r+1, 0LL);
+        for(int i=0; i<n; i++){
+            if(power < minPower){
+                long long need = minPower-power;
+                if(need > k) return 0;
+                k = k-need;
+                int pos = min(n-1, i+r);
+                stations[pos] = stations[pos]+need;
+                power = power+need;
+            }
+            if(i >= r) power = power-stations[i-r];
+            if(i+r+1 < n) power = power+stations[i+r+1];
+        }
+
+    return 1;
+    }
+
+    static long long maxPower(vector<int>& stations, int r, int k) {
+        const int n = stations.size();
+        long long left=0, right=reduce(stations.begin(), stations.end(), 0LL)+k;
+        long long res=0;
+        while(left <= right){
+            long long mid = left+(right-left)/2;
+            if(check(mid, stations, r, k, n)){
+                res = mid;
+                left = mid+1;
+            }
+            else right = mid-1;
+            
+        }
+
+    return res;
+    }
+};
