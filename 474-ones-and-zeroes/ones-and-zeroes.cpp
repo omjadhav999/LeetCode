20a2cb1df2622c8202961db272371b514debdd7b
Om Jadhav <jadhavom229@gmail.com>
2025-11-11 09:35:28 +0530

    Time: 54 ms (73.77%) | Memory: 13.7 MB (84.01%) - LeetSync

---
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        for(const string& s : strs){
+            int z=0, o=0;
+            for(char c : s) (c == '0') ? ++z : ++o;
+
+            for(int i=m; i>=z; --i){
+                for(int j=n; j>=o; --j){
+                    dp[i][j] = max(dp[i][j], dp[i-z][j-o]+1);
+                }
+            }
+        }
+
+    return dp[m][n];
+    }
+};
